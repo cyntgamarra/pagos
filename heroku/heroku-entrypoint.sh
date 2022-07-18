7c7f0372a9472742acb54e2e2a628f415bbe570b
luciakasman <lucia.kasman@muttdata.ai>
2022-07-18 02:37:02 -0300

    add datadog metrics

--- heroku/heroku-entrypoint.sh
+++ heroku/heroku-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-datadog-agent run > /dev/null &
-/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
-/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
-npm start
+datadog-agent run &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml
